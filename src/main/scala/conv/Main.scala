a7ca809fefc961e0bf781b6e823ae6a1eb6d3f60
Matthew Else <matthewelse1997@gmail.com>
2017-11-09 15:45:01 +0000

    Generate verilog source in Main.scala

--- src/main/scala/conv/Main.scala
+++ src/main/scala/conv/Main.scala
@@ -1,5 +1,34 @@
+// See LICENSE for license details.
 package conv
 
-object Main {
+import chisel3.{Driver, Module}
+import java.io.{File, PrintWriter}
 
+import firrtl.{CircuitState, ChirrtlForm, VerilogCompiler}
+
+object Builder {
+  def build(name: String, device: (() => Module)): Unit = {
+    // find the build directory
+    val dir = new File("./build").getCanonicalFile
+    dir.mkdirs()
+
+    // parse rtl
+    val drv = Driver.emit(device)
+    val nets = firrtl.Parser.parse(drv)
+
+    val verilogOut = dir.toPath.resolve(s"${name}.v").toString
+    val writer = new PrintWriter(verilogOut)
+    val compiler = new VerilogCompiler
+
+    val result = compiler.compileAndEmit(CircuitState(nets, ChirrtlForm))
+
+    writer.write(result.getEmittedCircuit.value)
+    writer.close()
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    Builder.build("simple_convolution", { () => new SimpleConvolution })
+  }
 }
