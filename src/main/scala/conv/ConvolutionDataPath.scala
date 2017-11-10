7891eb4c360505f30d8bb537db4abd0d38e5c3c4
Matthew Else <matthewelse1997@gmail.com>
2017-11-10 19:37:06 +0000

    Initial implementation of convolution data path

---
+++ src/main/scala/conv/ConvolutionDataPath.scala
@@ -0,0 +1,81 @@
+// See LICENSE for license details.
+package conv
+
+import chisel3._
+import chisel3.util.ValidIO
+
+/**
+  * Pipelined multiply/accumulate operations to speed up convolutions.
+  *           ┌───┐         ┌───┐         ┌───┐         ┌───┐         ┌───┐         ┌───┐         ┌───┐         ┌───┐
+  * p ┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┐
+  * i ┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┐ │   │       ┆
+  * x ┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┐ │   │       ┆ │   │       ┆
+  * e ┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┐ │   │       ┆ │   │       ┆ │   │       ┆
+  * l ┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┐ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆
+  * s ┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┐ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆
+  *   ┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┐ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆
+  * i ┈┈┈╱┈┈┈┈┤   ├┈┈┈┈╱┈┈┐ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆
+  * n ┈┈┈╱┈┈┐ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆ │   │       ┆
+  *         ┆ └─^─┘       ┆ └─^─┘       ┆ └─^─┘       ┆ └─^─┘       ┆ └─^─┘       ┆ └─^─┘       ┆ └─^─┘       ┆ └─^─┘       ┆
+  *         ┆             ┆             ┆             ┆             ┆             ┆             ┆             ┆             ┆
+  *        ┌┴┐  ┌─┐      ┌┴┐  ┌─┐      ┌┴┐  ┌─┐      ┌┴┐  ┌─┐      ┌┴┐  ┌─┐      ┌┴┐  ┌─┐      ┌┴┐  ┌─┐      ┌┴┐  ┌─┐      ┌┴┐  ┌─┐
+  *    w0┈┈┤*├┈┈┤+├┐ w1┈┈┤*├┈┈┤+├┐ w2┈┈┤*├┈┈┤+├┐ w3┈┈┤*├┈┈┤+├┐ w4┈┈┤*├┈┈┤+├┐ w5┈┈┤*├┈┈┤+├┐ w6┈┈┤*├┈┈┤+├┐ w7┈┈┤*├┈┈┤+├┐ w8┈┈┤*├┈┈┤+├┈┈ pixel_out
+  *        └─┘  └┬┘┆     └─┘  └┬┘┆     └─┘  └┬┘┆     └─┘  └┬┘┆     └─┘  └┬┘┆     └─┘  └┬┘┆     └─┘  └┬┘┆     └─┘  └┬┘┆     └─┘  └┬┘
+  *              0 ┆    ┌─┐    ┆ ┆    ┌─┐    ┆ ┆    ┌─┐    ┆ ┆    ┌─┐    ┆ ┆    ┌─┐    ┆ ┆    ┌─┐    ┆ ┆    ┌─┐    ┆ ┆    ┌─┐    ┆
+  *                └┈┈┈┈┤ ├┈┈┈┈┘ └┈┈┈┈┤ ├┈┈┈┈┘ └┈┈┈┈┤ ├┈┈┈┈┘ └┈┈┈┈┤ ├┈┈┈┈┘ └┈┈┈┈┤ ├┈┈┈┈┘ └┈┈┈┈┤ ├┈┈┈┈┘ └┈┈┈┈┤ ├┈┈┈┈┘ └┈┈┈┈┤ ├┈┈┈┈┘
+  *                     └^┘           └^┘           └^┘           └^┘           └^┘           └^┘           └^┘           └^┘
+  *
+  * @param convolutionSize total number of weights in each convolution operation.
+  * @param pixelSize number of bits
+  */
+class ConvolutionDataPath(convolutionSize: Int, pixelSize: Int) extends Module {
+  val io = IO(new Bundle {
+    val pixels_in = Flipped(new ValidIO(Vec(convolutionSize, SInt(pixelSize.W))))
+    val pixel_out = new ValidIO(SInt(pixelSize.W))
+
+    val weights = Input(Vec(convolutionSize, SInt(pixelSize.W)))
+  })
+
+  private val pixelShiftRegister = Seq.tabulate(convolutionSize) (n => {
+    RegInit(VecInit(Seq.fill(convolutionSize - n) {
+      0.S(pixelSize.W)
+    }))
+  })
+
+  private val validShiftRegister = RegInit(
+    VecInit(Seq.fill(convolutionSize) {
+      false.B
+    })
+  )
+
+  private val macs = Seq.fill(convolutionSize) {
+    Module(new MultiplyAccumulator(pixelSize.W, pixelSize.W))
+  }
+
+  for (i <- 0 until convolutionSize) {
+    pixelShiftRegister.head(i) := io.pixels_in.bits(i)
+  }
+
+  macs(0).io.partial := 0.S
+  validShiftRegister(0) := io.pixels_in.valid
+
+  // Setup the data path
+  for (i <- 0 until convolutionSize) {
+    // TODO: how to handle changing the weights?
+    macs(i).io.weight := io.weights(i)
+    macs(i).io.pixel <> pixelShiftRegister(i)(0)
+
+    if (i != convolutionSize - 1) {
+      macs(i).io.partial_out <> macs(i + 1).io.partial
+      validShiftRegister(i + 1) := validShiftRegister(i)
+
+      // connect up the pixel shift register
+      for (j <- 0 until (convolutionSize - i - 1)) {
+        pixelShiftRegister(i + 1)(j) := pixelShiftRegister(i)(j + 1)
+      }
+    } else {
+      io.pixel_out.bits <> macs(i).io.partial_out
+      io.pixel_out.valid := validShiftRegister(convolutionSize - 1)
+    }
+  }
+}
